0a1cbb327f108bf9a869ed7cdfd86f6377600a16
Real.mo <czb166@qq.com>
2017-05-25 00:07:29 +0800

    BaseType
    BaseType 基本类型

---
+++ BaseType.kt
@@ -0,0 +1,43 @@
+/**
+ * Created by RealMo on 2017-05-24.
+ */
+fun main(args: Array<String>) {
+    /**
+     * 类型      位宽
+     * Double           64
+     * Float            32
+     * Long             64
+     * Int              32
+     * Short            16
+     * Byte             8
+     * Char
+     * Bool
+     * String
+     *
+     * 每个数值类型都支持下面的转换：
+
+        toByte(): Byte
+        toShort(): Short
+        toInt(): Int
+        toLong(): Long
+        toFloat(): Float
+        toDouble(): Double
+        toChar(): Char
+     *
+     *
+     *
+     * Kotlin不支持8进制
+     * 10进制：123
+     * 16进制：0x0f
+     * 二进制：0b00001010
+     *
+     *
+     * 数值常量可以添加下划线分割  for   增强可读性
+     * for example:
+     * val oneMillion = 1_000_000
+     * val securityNumber = 999_99_999L
+     *
+     *
+     *
+     */
+}
