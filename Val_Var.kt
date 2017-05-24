b37231c3c6b720d08b105c1570031b0ce9f7d4eb
Real.mo <czb166@qq.com>
2017-05-24 23:44:36 +0800

    Val_Var
    Val_Var 常量变量

---
+++ Val_Var.kt
@@ -0,0 +1,21 @@
+/**
+ * Created by RealMo on 2017-05-24.
+ */
+fun main(args: Array<String>) {
+    //常量val
+    val a : Int = 1 //立即初始化
+    val b =2    //推导出Int型
+    val c:Int   //没有初始化值时必须声明类型
+    c =3    //赋值
+    println("a = $a, b = $b, c = $c")
+
+    //变量var
+    var x = 5
+    x +=1
+    println("x = $x")
+
+
+
+
+}
+
