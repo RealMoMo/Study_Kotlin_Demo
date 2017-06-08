48a980b1739d0941013f5a5a22635ff45223e3ce
Real.mo <czb166@qq.com>
2017-06-08 23:21:19 +0800

    commit
    commit

--- BaseType.kt
+++ BaseType.kt
@@ -1,8 +1,16 @@
 /**
- * Created by RealMo on 2017-05-24.
+ * Created by RealMo on 2017-05-24
+ * Content:Learn Basic Tpyes
  */
+
+/*
+* In Kotlin,everthing is an object in the sense that
+* we can call member functuions and properties on any variable.
+* */
 fun main(args: Array<String>) {
     /**
+     *
+     * Numbers
      * 类型      位宽
      * Double           64
      * Float            32
@@ -10,19 +18,9 @@ fun main(args: Array<String>) {
      * Int              32
      * Short            16
      * Byte             8
-     * Char
-     * Bool
-     * String
      *
-     * 每个数值类型都支持下面的转换：
-
-        toByte(): Byte
-        toShort(): Short
-        toInt(): Int
-        toLong(): Long
-        toFloat(): Float
-        toDouble(): Double
-        toChar(): Char
+     * Note that characters are not numbers in Kotlin.
+     *
      *
      *
      *
@@ -40,4 +38,59 @@ fun main(args: Array<String>) {
      *
      *
      */
+
+
+        /*
+        * Representation(表示)
+        *
+        *
+        *       Note that boxing of numbers does not necessarily preserve identity:
+        *
+        *       val a: Int = 10000
+                print(a === a) // Prints 'true'
+                val boxedA: Int? = a
+                val anotherBoxedA: Int? = a
+                print(boxedA === anotherBoxedA) // !!!Prints 'false'!!!
+
+                On the other hand, it preserves equality:
+                val a: Int = 10000
+                print(a == a) // Prints 'true'
+                val boxedA: Int? = a
+                val anotherBoxedA: Int? = a
+                print(boxedA == anotherBoxedA) // Prints 'true'
+        *
+        * */
+
+        /*
+        * Explicit Conversions(显式转换)
+        * Due to different representations, smaller types are not subtypes of bigger ones. If they were, we would have troubles of the
+        * following sort:
+        *
+        *       // Hypothetical code, does not actually compile:
+                val a: Int? = 1 // A boxed Int (java.lang.Integer)
+                val b: Long? = a // implicit conversion yields a boxed Long (java.lang.Long)
+                print(a == b) // Surprise! This prints "false" as Long's equals() check for other part to be Long as well
+
+                As a consequence, smaller types are NOT implicitly converted to bigger types. This means that we cannot assign a value of type
+                Byte to an Int variable without an explicit conversion
+                所以，短类型是不会隐式转换为长类型的。这意味着我们必须显式转换才能把 Byte 赋值给 Int
+
+                val b: Byte = 1 // OK, literals are checked statically
+                val i: Int = b // ERROR
+
+                we can use explicit conversion to widen numbers
+                val i:Int = b.toInt() // ok:explicitly widened
+        * */
+/*
+*      每个数值类型都支持下面的转换：
+
+        toByte(): Byte
+        toShort(): Short
+        toInt(): Int
+        toLong(): Long
+        toFloat(): Float
+        toDouble(): Double
+        toChar(): Char
+* */
+
 }
---
+++ Operations.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by RealMo on 2017-06-08.
+ * Content:Learn Operations(运算符)
+ */
+fun main(args: Array<String>) {
+
+
+}
